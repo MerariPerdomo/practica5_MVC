df12647bbdc64ceb83e096d114c970f30f40776a
angcm25 <merari.perdomo@catolica.edu.sv>
2023-03-25 11:01:26 -0600

    Agregar archivos de proyecto.

---
+++ Models/equiposDbcontext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace practicaMVC.Models
+
+{
+    public class equiposDbcontext : DbContext
+    {
+        public equiposDbcontext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<marcas> marcas { get; set; }
+    }
+}
